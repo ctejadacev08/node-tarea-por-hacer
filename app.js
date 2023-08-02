0dc43de78c0f91eb48979590684f3cec996ee5cf
Christian <tejada.utn08@hotmail.com>
2023-08-01 22:57:05 -0500

    agregando el paquete uuid para crear tareas

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 //const { menuPrincipal, pausa } = require("./helpers/menu");
 
 const { menuInquirer, pausa } = require("./helpers/inquirerMenu");
+const Tarea = require("./models/tarea");
+const Tareas = require("./models/tareas");
 
 const main = async () => {
   let op = "";
   do {
-    op = await menuInquirer();
-    console.log({ op });
+    // op = await menuInquirer();
+    // console.log({ op });
+    const tarea = new Tarea("preparar comida");
+    const tareas = new Tareas();
+    tareas._listado[tarea.id] = tarea;
+    console.log(tareas);
     await pausa();
   } while (op !== "0");
 };
---
+++ models/tareas.js
@@ -0,0 +1,15 @@
+/* _listado: {
+    // { "uuid-1234568-45658452-2:{id:12,desc:asd,completadoEn:9251}" },-->comentar un sola
+    // linea de codigo: ctrl +}
+    { "uuid-1234568-45658452-2:{id:12,desc:asd,completadoEn:9251}" },
+    { "uuid-1234568-45658452-2:{id:12,desc:asd,completadoEn:9251}" }
+  }---> seleccionar un bloque de codigo: alt+shift+a */
+
+class Tareas {
+  _listado = {};
+  constructor() {
+    this._listado = {};
+  }
+}
+
+module.exports = Tareas;
